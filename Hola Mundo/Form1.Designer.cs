261da628597024aa4b185681b1a3dff30bd2a5f5
luisloo10 <https://luisloo10@bitbucket.org>
2020-05-01 19:39:30 -0600

    mi primer commit

---
+++ Hola Mundo/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Hola_Mundo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bHola = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bHola
+            // 
+            this.bHola.Location = new System.Drawing.Point(12, 25);
+            this.bHola.Name = "bHola";
+            this.bHola.Size = new System.Drawing.Size(75, 23);
+            this.bHola.TabIndex = 0;
+            this.bHola.Text = "Hola";
+            this.bHola.UseVisualStyleBackColor = true;
+            this.bHola.Click += new System.EventHandler(this.bHola_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 381);
+            this.Controls.Add(this.bHola);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ventana";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bHola;
+    }
+}
+
