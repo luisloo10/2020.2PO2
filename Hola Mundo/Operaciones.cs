7e29997bd9585c669a05d3bd4abd9f1c7a796bb9
luisloo10 <https://luisloo10@bitbucket.org>
2020-05-05 20:02:57 -0600

    Agregando clases operaciones y suma

---
+++ Hola Mundo/Operaciones.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hola_Mundo
+{
+    public class Operaciones
+    {
+        public int Valor1;
+        public int Valor2;
+
+    }
+
+    public class Suma : Operaciones
+    {
+        public int Valor3;
+    }
+}
---
+++ Hola Mundo/fOperaciones.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hola_Mundo
+{
+    public partial class fOperaciones : Form
+    {
+
+        public fOperaciones()
+        {
+            InitializeComponent();
+        }
+
+        private void fOperaciones_Load(object sender, EventArgs e)
+        {
+            Operaciones Op = new Operaciones();
+            Suma Sm = new Suma();
+            
+        }
+    }
+}
