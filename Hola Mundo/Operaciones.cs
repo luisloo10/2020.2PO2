d88b68c2e8c18440cb46c2be833027377bba786f
Cristhian Gonzalez <ce.gonzalez@ujn.edu.hn>
2020-05-08 17:50:04 -0600

    Se agrego la clase DobleMultiplicacion

--- Hola Mundo/Operaciones.cs
+++ Hola Mundo/Operaciones.cs
@@ -62,6 +62,20 @@ namespace Hola_Mundo
     }
 }
 
+namespace Hola_Mundo
+{
+    class DobleMultiplicacion: Operaciones
+    {
+        public int Operar
+        {
+            protected set
+            {
+                resultado = 2(valor1*valor2); 
+            }
+            }
+        }
+    }
+
  
 
 
