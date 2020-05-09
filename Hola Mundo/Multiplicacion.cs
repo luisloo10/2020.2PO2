4158e408cb7ecfbc16c4912d97406cf7df0a886c
marlpaz <marlpaz@VNS-B8QY7S2>
2020-05-08 18:38:53 -0600

    Funcion de multiplicacion

---
+++ Hola Mundo/Multiplicacion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hola_Mundo
+{
+    class Multiplicacion : Operaciones
+    {
+        public int Operar
+        {
+            protected set
+            {
+                resultado = valor1 * valor2;
+            }
+            get
+            {
+                return resultado;
+            }
+        }
+    }
+}
