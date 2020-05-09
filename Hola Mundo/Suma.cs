5909ae703bac0a9cd3dcf28d6c8fbe8fc5ddaee9
franklin-mesquite <64745475+franklin-mesquite@users.noreply.github.com>
2020-05-08 19:17:09 -0600

    Agregado de Suma

---
+++ Hola Mundo/Suma.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hola_Mundo
+{
+    class Suma : Operaciones
+    {
+        int operar()
+        {
+            return resultado = valor1 + valor2;
+        }
+    }
+}
