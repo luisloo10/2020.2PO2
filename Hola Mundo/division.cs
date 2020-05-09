f83b8752b26b760ed0af85d5dcc0459424faa00d
Javier Diaz <Javier Diaz@DESKTOP-KUI6878>
2020-05-08 18:30:15 -0600

    Clase division

---
+++ Hola Mundo/division.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hola_Mundo
+{
+    class division : Operaciones
+
+    {
+
+        int operar()
+            {
+            return resultado = valor1 / valor2;
+            }
+    }
+}
